1f75b1101c7c3140588dbfad80fe4ca537d20d17
sihyeong <parksh4603@naver.com>
2026-05-17 16:59:46 +0900

    initial devnote webapp setup

---
+++ devnote-webapp/src/main/java/io/hyeongsi/devnotewebapp/DevnoteWebappApplication.java
@@ -0,0 +1,13 @@
+package io.hyeongsi.devnotewebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevnoteWebappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevnoteWebappApplication.class, args);
+    }
+
+}
